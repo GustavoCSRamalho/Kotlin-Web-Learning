b31a1a815b659692294c13fb8768a05c8788cefb
Gustavo Ramalho <gstvramalho@gmail.com>
2019-09-15 22:33:57 -0300

    List function created

--- src/main/kotlin/br/com/gustavo/ceepws/controller/NoteController.kt
+++ src/main/kotlin/br/com/gustavo/ceepws/controller/NoteController.kt
@@ -1,13 +1,19 @@
 package br.com.gustavo.ceepws.controller
 
+import br.com.gustavo.ceepws.model.Note
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 @RequestMapping("notes")
 class NoteController {
 
     @GetMapping
-    fun list() {}
+    @ResponseBody
+    fun list(): List<Note> {
+        return listOf(Note("Leitura","Livro de Spring Boot"),
+                Note("Pesquisa","Ambiente com Docker"))
+    }
 }
